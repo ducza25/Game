fb63b9656cb43d2972c8d028ed041daa8e7bc24f
ducza25 <120005798+ducza25@users.noreply.github.com>
2023-12-12 06:30:54 +0100

    Update script.js
    Script/jatek bővítése

--- script.js
+++ script.js
@@ -1,15 +1,31 @@
 const gamea = document.querySelector('#gamea')
 
 
+let t=[]
+for(let i=0;i<100;i++){
+    t.push(i+1)
+}
+
+    
+for(let i=0; i<100; i++){
+let pos1= Math.floor(Math.random()*12)
+let pos2= Math.floor(Math.random()*12)
+let temp=t[pos1]
+t[pos1]=t[pos2]
+t[pos2]=temp
+}
+
+let nextNumber = 1
 
 for (let i= 0; i < 12; i++) {
-    let szamdoboz;
-    szamdoboz = document.createElement ('div')
-szamdoboz.innerHTML = i + 1
-gamea.appendChild(szamdoboz)
-
-szamdoboz.addEventListener('click',function (){
-  this.classList.add('rejtett')
+    let szamDoboz = document.createElement ('div')
+    szamDoboz.innerText=t[i] 
+    gamea.appendChild(szamdoboz)
+    szamDoboz.addEventListener('click',function() {
+    if (szamDoboz.innerText== nextNumber){
+        szamDoboz.classList.add('rejtett')
+        nextNumber++
+    }
 })
-    
+
 }
